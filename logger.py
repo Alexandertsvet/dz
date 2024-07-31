d64b8f5c0cccdb0aef206c850ef788839e334f62
unknown <tsvetkov1986@mail.ru>
2024-07-31 17:39:48 +0300

    logger

--- logger.py
+++ logger.py
@@ -9,12 +9,27 @@ def input_data():
     f'1 первый вариант\n'
     f'{name}\n{surname}\n{phone}\n{address}\n\n'
     f'2 второй вариант\n'
-    f'{name};{surname};{phone};{address};\n'
+    f'{name};{surname};{phone};{address};\n\n'
     f'Выберите вариант.'
     ))
+    while var!=1 and var!=2:
+        print('Неправильный ввод!Введите значение 1 или 2.')
+        command = int(input('Введите число'))
+
+    if var == 1:
+        with open('data_first_variant.csv', 'a', encoding='utf-8') as f:
+            f.write(f'{name}\n{surname}\n{phone}\n{address}\n\n')
+    elif var == 2:
+        with open('data_second_variant.csv', 'a', encoding='utf-8') as f:
+            f.write(f'{name};{surname};{phone};{address};\n\n')
+
 
 def print_data():
-    pass
+    print('Вывод данных из первого файла.')
+    with open('data_first_variant.csv', 'r', encoding='utf-8') as f:
+            data_first = f.readlines()
+            
+                
 
 
-input_data()
+print_data()
